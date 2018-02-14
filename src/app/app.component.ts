1c262fe6a02e18c91607cca5650d3f278bff48a8
chad.wescott <chad.wescott@cloudcheckr.com>
2018-02-14 14:26:02 -0500

    Added a player entry component to wrap up the player selection and form components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,28 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Score Keeper';
-  numberOfPlayers = 2;
-  players: string[] = ['', ''];
-
-  updateNumberOfPlayers(numberOfPlayers: number) {
-    if (numberOfPlayers > this.numberOfPlayers) {
-      this.increaseNumberOfPlayers(numberOfPlayers);
-    }
-    if (numberOfPlayers < this.numberOfPlayers) {
-      this.decreaseNumberOfPlayers(numberOfPlayers);
-    }
-    this.numberOfPlayers = numberOfPlayers;
-  }
-
-  private increaseNumberOfPlayers(numberOfPlayers: number) {
-    for (let i = this.numberOfPlayers; i < numberOfPlayers; i ++) {
-      this.players.push('');
-    }
-  }
-
-  private decreaseNumberOfPlayers(numberOfPlayers: number) {
-    for (let i = numberOfPlayers; i < this.numberOfPlayers; i ++) {
-      this.players.pop();
-    }
-  }
 }
